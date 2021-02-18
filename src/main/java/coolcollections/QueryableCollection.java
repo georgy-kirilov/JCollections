0095fac60d3234c6cab15c76c50bdae05fb94e1c
georgy-kirilov <georgynk17@gmail.com>
2021-02-18 17:42:35 +0200

    Create QueryableCollection

---
+++ src/main/java/coolcollections/QueryableCollection.java
@@ -0,0 +1,70 @@
+package coolcollections;
+
+import delegates.actions.TinyAction;
+import delegates.functions.Predicate;
+import delegates.functions.TinyFunc;
+
+import java.util.Iterator;
+
+public abstract class QueryableCollection<TSource> implements Queryable<TSource>
+{
+    @Override
+    public int count()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean all(Predicate<TSource> filter)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean any(Predicate<TSource> filter)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TSource first(Predicate<TSource> filter)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Queryable<TSource> where(Predicate<TSource> filter)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <TOut> Queryable<TSource> select(TinyFunc<TSource, TOut> selector)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Queryable<TSource> foreach(TinyAction<TSource> action)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <TKey extends Comparable<TKey>> Queryable<TSource> sortBy(TinyFunc<TSource, TKey> keySelector)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <TKey extends Comparable<TKey>> Queryable<TSource> sortByDesc(TinyFunc<TSource, TKey> keySelector)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<TSource> iterator()
+    {
+        throw new UnsupportedOperationException();
+    }
+}
--- src/main/java/coolcollections/lists/List.java
+++ src/main/java/coolcollections/lists/List.java
@@ -2,6 +2,18 @@ package coolcollections.lists;
 
 import coolcollections.Queryable;
 
-public interface List<T> extends Queryable<T>
+public interface List<T>
 {
+    void add(T item);
+    void addAll(Iterable<T> collection);
+    void insert(int index, T item);
+    T get(int index);
+    void set(int index, T item);
+    boolean remove(T item);
+    T removeAt(int index);
+    int indexOf(T item);
+    void clear();
+    boolean contains(T item);
+    boolean containsAll(Iterable<T> collection);
+    List<T> subList(int startIndex, int endIndex);
 }
